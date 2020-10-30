8dfe96bc7ced2d366336f5d3cf8de26e23cecf6f
RafikiSalmeron <rafikisalmeronmartos@gmail.com>
2020-10-30 12:36:36 +0100

    Pruebas de clase

--- main.js
+++ main.js
@@ -13,13 +13,16 @@ function startup() {
 
 window.onload = startup;
 
+var coords;
+
 function handleStart(evt) {
     evt.preventDefault();
     document.getElementById('log').innerHTML = "TouchStart " + evt.touches[0].pageX + " " + evt.touches[0].pageY;
+    coords = evt.touches[0].pageX + " " + evt.touches[0].pageY;
 }
 function handleEnd(evt) {
     evt.preventDefault();
-    document.getElementById('log').innerHTML = "TouchEnd " + evt.touches[0].pageX + " " + evt.touches[0].pageY;
+    document.getElementById('log').innerHTML = "TouchEnd " + coords;
 }
 function handleCancel(evt) {
     evt.preventDefault();
@@ -28,6 +31,7 @@ function handleCancel(evt) {
 function handleMove(evt) {
     evt.preventDefault();
     document.getElementById('log').innerHTML = "TouchMove " + evt.touches[0].pageX + " " + evt.touches[0].pageY;
+    coords = evt.touches[0].pageX + " " + evt.touches[0].pageY;
 }
 function click(){
     alert('Click');
